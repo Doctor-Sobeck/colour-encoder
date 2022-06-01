fbe91893400f27247d52c3a6324ac4edc50bed03
Doctor-Sobeck <73001976+Doctor-Sobeck@users.noreply.github.com>
2022-06-02 09:49:34 +1200

    Update colour.py

--- colour.py
+++ colour.py
@@ -1,10 +1,13 @@
 import math
+
 def encodeColour(r,g,b):
 	# colour = (0 - 255)
 	return (r+g*256+b*65536)
 
 def unencodeColour(colour):
-	r = colour%256
-	g = (colour-r)%65536/256
-	b = ((colour-g)-r)/65536
-	return (math.floor(r),math.floor(g),math.floor(b))
+	# Set each colour value 
+	r = math.floor(colour%256)
+	g = math.floor((colour-r)%65536/256)
+	b = math.floor((colour-g-r)/65536)
+	
+	return (r,g,b)
